0a77af351e6049d62d2b69a681234cade8070436
Vigo Wei <vigowei@wustl.edu>
2013-11-23 15:14:57 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BearBooks::Application.config.secret_key_base = '9643ca6a8b5b539cdefaf811354e91b2644624bba886dd2abb57201139d5f6c7e6a0eb470b52c4c7288f25208a82931d5d9f1a5896d3c7f7bbf61ae583a9fb75'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BearBooks::Application.config.session_store :cookie_store, key: '_bearBooks_session'
