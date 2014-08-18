29749d89046ede89aa9945d3f35502363b5b5960
Vigo Wei <vigowei@wustl.edu>
2014-08-18 17:04:10 -0500

    added comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 require 'parse-ruby-client'
 Parse.init :application_id => "e6Sd3RCndeGV6p2T7nxqDIo5rOfV4F13F8ruS4lU",
            :api_key        => "KNgVPhsP2CsfRINBZAEUlshOPnvtYozEdW9rOMD4"
+# Parse is used to sync book data between mobile and web platforms
+
 
 class UsersController < ApplicationController
   before_filter :authenticate_user!
@@ -10,6 +12,7 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  #the page that displays social requests
   def usersPool
     @requests = Parse::Query.new("Requests").eq("responder", current_user.id).get
     @friends = Parse::Query.new("Friends").eq("user_id", current_user.id).get
@@ -17,8 +20,11 @@ class UsersController < ApplicationController
     @user = User.find(current_user.id)
   end
 
+  #show the user's profile including their books, friends and comments from others
   def show
     @users = User.all
+
+    #includes 3 queries to Parse for relevant info
     query = Parse::Query.new("Books")
     @books = query.get
     @user = User.find(params[:id].delete('/').to_i)
@@ -28,10 +34,13 @@ class UsersController < ApplicationController
     @friends = friendsQuery.get
     commentsQuery = Parse::Query.new("Comments").eq("user_id", @user.id)
     @comments = commentsQuery.get
+
+    #search & display books without leaving the page
     @results = Array.new
     @results = @books.select { |s| s["title"].include? params[:searchToken] } if !params[:searchToken].nil?
   end
-  
+
+  # simple method to update user info
   def update
     authorize! :update, @user, :message => 'Not authorized as an administrator.'
     @user = User.find(params[:id])
@@ -42,6 +51,7 @@ class UsersController < ApplicationController
     end
   end
 
+  # retrieves parameters from form, stores everything in the book table (Parse)
   def uploadBook
     new_book = Parse::Object.new("Books")
     new_book["title"] = params[:title]
@@ -55,6 +65,7 @@ class UsersController < ApplicationController
     redirect_to user_path(current_user), :notice => current_user.id
   end
 
+  # deletes a book (warning displayed on :hover delete button)
   def deleteBook
     query = Parse::Query.new("Books")
     query.eq("user_id", current_user.id)
@@ -63,6 +74,8 @@ class UsersController < ApplicationController
     redirect_to user_path(current_user), :notice => "deleted"
   end
 
+  # when the user likes a book, up the like index by 1
+  # TODO: restrict votes per IP/user.
   def likeBook
     like = Parse::Object.new("Likes")
     like["book_id"] = params[:book_id]
@@ -71,6 +84,7 @@ class UsersController < ApplicationController
     redirect_to user_path(params[:user_id].to_i), :notice => "you liked the book."
   end
 
+  #send a request to a user (unresponded reqs stored in table)
   def addFriend
     request = Parse::Object.new("Requests")
     request["initiator"] = params[:initiator].to_i
@@ -79,6 +93,7 @@ class UsersController < ApplicationController
     redirect_to user_path(params[:responder]), :notice =>"Request Sent"
   end
 
+  # delete a friend (essentially destroys the 1-1 relationship thru js)
   def deFriend
     query = Parse::Query.new("Friends").eq("user_id",current_user.id)
     @friendsA = query.get
@@ -86,6 +101,8 @@ class UsersController < ApplicationController
     @friendsB = queryB.get
   end
 
+  # add a friend (add a FK to each other)
+  # not the best solution due to the lack of API support for rails
   def acceptReq
     friend = Parse::Object.new("Friends")
     friend["user_id"] = current_user.id
@@ -95,6 +112,8 @@ class UsersController < ApplicationController
     friendB["friend_id"] = current_user.id
     friendB["user_id"] = params[:initiator].to_i
     friendB.save
+
+    #after adding the friend, delete the request
     @requests = Parse::Query.new("Requests").eq("initiator", params[:initiator].to_i).get
     @requests.each do |r|
       if r["responder"] == current_user.id
@@ -104,6 +123,7 @@ class UsersController < ApplicationController
     redirect_to "/usersPool", :notice =>"You guys are now friends :)"
   end
 
+  #properly handles declined friend requests
   def declineReq
     @requests = Parse::Query.new("Requests").eq("initiator", params[:initiator].to_i).get
     @requests.each do |r|
@@ -111,9 +131,10 @@ class UsersController < ApplicationController
         r.parse_delete
       end
     end
-    redirect_to "/usersPool", :notice =>"Okay, you turned them down but we won't tell others :("
+    redirect_to "/usersPool", :notice =>"Okay, you turned them down but we won't tell"
   end
 
+  # adds a comment to the comment table (Parse)
   def addComment
     friend = Parse::Object.new("Comments")
     friend["commenter_id"] = current_user.id
@@ -123,6 +144,7 @@ class UsersController < ApplicationController
     redirect_to user_path(params[:user_id]), :notice =>"Comment added, please be nice and kind to each other ;)"
   end
 
+  # deletes a user (only admin allowed)
   def destroy
     authorize! :destroy, @user, :message => 'Not authorized as an administrator.'
     user = User.find(params[:id])
